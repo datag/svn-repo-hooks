6416df8a19009d34ed686dae4edde8624bcb35df
Dominik D. Geyer <dominik.geyer@gmail.com>
2013-09-29 15:23:39 +0200

    Refactoring

--- server/hooks/pre-commit.sh
+++ server/hooks/pre-commit.sh
@@ -3,41 +3,76 @@
 # no/short message
 # big file
 # temp file
-# alter tag
-#
-# allow by string: i know what i'm doing
-# trim empty lines
+# prevent alter tag
 
-
+################################################################################
 REPOS="$1"	# Absolute repository path
 TXN="$2"	# Transaction ID of pending commit
+################################################################################
 
-LOGMSG=$($SVNLOOK log -t "$TXN" "$REPOS") || errexit "Log message konnte nicht ermittelt werden."
-LOGMSG=$(trim "$LOGMSG")
+RULE_logmessage_minlength=1
+RULE_newfile_maxsize=$((5 * 1024*1024))
 
-# CHECK log message
-if (( ${#LOGMSG} < $RULE_logmsg_min_length )) && ! has_force "$LOGMSG" FORCE_MSG; then
-	status 10
-	msg "Regel: Die log message sollte mindestens $RULE_logmsg_min_length Zeichen lang sein."
-	msg "Erzwingen mit: !FORCE_MSG"
-fi
+function svnlook_txn()
+{
+	declare cmd=$1; shift
+	
+	$SVNLOOK $cmd --transaction "$TXN" "$REPOS" $@
+}
 
-# TEST for filesize of new/added files
-while read -r line; do
-	##msg "DEBUG: $line"
-	# file has been added (new file "A   ", not copied "A + ")
-	if { egrep '^A\s{3}' &>/dev/null <<<"$line"; } && ! has_force "$LOGMSG" FORCE_FILESIZE; then
-		filepath=${line:4}
-		filesize=$($SVNLOOK filesize -t "$TXN" "$REPOS" "$filepath") || errexit "Dateigröße konnte nicht ermittelt werden."
-		
-		if (( $filesize > $RULE_add_filesize_max )); then
-			status 20
-			msg "Regel: Dateigröße einer neuen Datei sollte kleiner/gleich $(byteshuman $RULE_add_filesize_max) sein."
-			msg "Datei '$filepath' mit $(byteshuman $filesize)"
-			msg "Erzwingen mit: !FORCE_FILESIZE"
-		fi
+function reject()
+{
+	declare -i rc=${1:-1}
+	
+	(( rc > 0 )) && STATUS=rc || :
+}
+
+function has_force()
+{
+	declare rule=$1
+	egrep -q -w '!'${rule} &>/dev/null <<<"$LOGMSG"
+}
+
+function rule_logmessage()
+{
+	declare cinfo=$(svnlook_txn info) || errexit "Could not get transaction info."
+	declare -i cmsglen=$(sed -n -e '3{p;q}' <<<"$cinfo")
+	
+	if (( cmsglen < RULE_logmessage_minlength )) && ! has_force FORCE_LOGMESSAGE; then
+		reject 10
+		msg " RULE: The log message ($cmsglen characters) should have at least $RULE_logmessage_minlength characters."
+		msg "FORCE: !FORCE_LOGMESSAGE"
+		msg
 	fi
-done < <($SVNLOOK changed -t "$TXN" --copy-info "$REPOS")
+}
 
-return 0
+function rule_bigfiles()
+{
+	declare line
+	
+	while read -r line; do
+		##msg "DEBUG: $line"
+		# file has been added (new file "A   ", not copied "A + ")
+		if { egrep '^A\s{3}' &>/dev/null <<<"$line"; } && ! has_force FORCE_BIGFILES; then
+			declare filepath=${line:4}
+			declare -i filesize=$(svnlook_txn filesize "$filepath") || errexit "Could not get filesize of changed file."
+		
+			if (( filesize > RULE_newfile_maxsize )); then
+				reject 20
+				msg " RULE: The new file '$filepath' shouldn't be bigger than $(byteshuman $RULE_newfile_maxsize), but is $(byteshuman $filesize)."
+				msg "FORCE: !FORCE_BIGFILES"
+				msg
+			fi
+		fi
+	done < <(svnlook_txn changed --copy-info)
+}
 
+################################################################################
+
+declare -i STATUS=0
+LOGMSG=$(svnlook_txn log) || errexit "Could not get transaction log message."
+
+rule_logmessage
+rule_bigfiles
+
+return $STATUS
--- server/repo-hooks.sh
+++ server/repo-hooks.sh
@@ -3,17 +3,8 @@
 set -o errexit -o pipefail -o nounset
 
 SVNLOOK=/usr/bin/svnlook
-RULE_logmsg_min_length=50
-RULE_add_filesize_max=$((5 * 1024*1024))
 
 ################################################################################
-
-declare -i STATUS=0
-function status()
-{
-	declare -i s=${1:-1}
-	(( s > 0 )) && STATUS=$s || true
-}
 
 function msg()
 {
@@ -22,19 +13,8 @@ function msg()
 
 function errexit()
 {
-	echo "!!! Fehler: $@" >&2
+	echo "!!! Error: $@" >&2
 	exit 1
-}
-
-# borrowed from datag/pkgbox
-function trim()
-{
-	declare str=${1-""}
-	if [[ $str =~ [[:space:]]*([^[:space:]]|[^[:space:]].*[^[:space:]])[[:space:]]* ]]; then
-		echo -n "${BASH_REMATCH[1]}"
-	else
-		echo -n "$str"
-	fi
 }
 
 # borrowed from datag/pkgbox
@@ -44,20 +24,14 @@ function byteshuman()
 	awk -v x="$x" 'BEGIN { if (x<1024) { printf("%d Byte(s)", x) } else { split("KiB MiB GiB TiB PiB", t); while (x>=1024) { x/=1024; ++i }; printf("%.2f %s", x, t[i]) } }'
 }
 
-function has_force()
-{
-	declare log=${1-""} identifier=${2-""}
-	grep "!${identifier}" &>/dev/null <<<"$log"
-}
-
 ################################################################################
 
 SCRIPT_DIR=$(dirname "$(readlink -f "$BASH_SOURCE")")
 HOOK=${0##*/}
 HOOK_SCRIPT="${SCRIPT_DIR}/hooks/${HOOK}.sh"
 
-[[ -r "${HOOK_SCRIPT}" ]] || errexit "Konfigurationsfehler: Ungültiger hook '${HOOK}'."
+[[ -r "${HOOK_SCRIPT}" ]] || errexit "Configuration error: Invalid hook '${HOOK}'."
 
-source "${HOOK_SCRIPT}"	|| errexit "Hook '${HOOK}' gab einen Fehler zurück."
-exit $STATUS
+source "${HOOK_SCRIPT}"
+exit $?
 
